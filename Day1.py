89966e2a6fc57fb16f45c2cf5b6dfce987680be6
Dev Patel <devpatel@Devs-MacBook-Air.local>
2025-12-31 20:52:32 -0600

    Add Tip calculator and Band Name Generator scripts

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Hello users Welcome to Band name Generator.")
+city = input("Enter the name of city you grew up in?\n ")
+pet = input("Enter the name of your pet?\n ")
+print("Your band name could be:"+" "+city+" "+pet)

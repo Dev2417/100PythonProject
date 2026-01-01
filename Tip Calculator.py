a6ff238d9e7701a3a904a4bab0cfb6bb1735d315
Dev2417 <dev2412007@gmail.com>
2025-12-31 21:06:41 -0600

    Update Tip Calculator.py

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -3,4 +3,4 @@ bill = float(input("What was the total bill?\n"))
 tip = int(input("How much tip would you like to give 10 12 15?\n"))
 people = int(input("How many people are splitting the bill?\n"))
 cost = (bill*(tip/100+1))/people
-print(f"The total amount tompay for each person is=${round(cost,2)}")
+print(f"The total amount to pay for each person is=${round(cost,2)}")
